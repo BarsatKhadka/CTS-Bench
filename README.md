a38d1deb40c2ada924ccb2e44f0d42cf5da519ef
BarsatKhadka <khadkabarsat598@gmail.com>
2026-01-02 15:49:00 -0600

    00_once_per_design.py

---
+++ README.md
@@ -0,0 +1,4 @@
+#Steps to Reproduce
+# Run once per design for all designs first. 
+We run these variables on that file once per design. Those variables are pre placement , we do so , so that we strictly learn the effects of placement and nothing else.
+
--- scripts/01_gen_placement.py
+++ scripts/01_gen_placement.py
@@ -2,25 +2,29 @@ import os
 from openlane.flows import SequentialFlow
 from openlane.steps import Yosys
 
-# 1. Test Flow that does ONLY Synthesis
-class SynthesisOnlyFlow(SequentialFlow):
+
+
+
+
+# 1. Flow until placement. After Placement we run cts 10 times on the same placement.
+class flow_till_placement(SequentialFlow):
     Steps = [
-        Yosys.Synthesis
+        
     ]
 
 # 2. Config 
 config = {
     "DESIGN_NAME": "picorv32",
-    "VERILOG_FILES": ["./designs/src/picorv32a.v"],
+    "VERILOG_FILES": ["./designs/src/picorv32a/picorv32a.v"],
     "CLOCK_PORT": "clk",
-    "CLOCK_PERIOD": 10.0
+    "CLOCK_PERIOD": 16.0
 }
 
 # 3. Run 
 def run_test():
     print("--- Testing Yosys Synthesis Link ---")
     
-    flow = SynthesisOnlyFlow(
+    flow = flow_till_placement(
         config=config,
         design_dir=".",
         pdk="sky130A"
